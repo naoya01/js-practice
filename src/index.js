ddaee117bdc2c39504db7d08b0496dfbcfcf6ef4
naoya <93042220+naoya01@users.noreply.github.com>
2022-03-31 22:26:30 +0900

    変数宣言

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import "./styles.css";
+// const,letの変数宣言
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "変数を上書き";
+// console.log(val1);
+
+// // var変数は際宣言可能
+// var val1 = "var祭宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2)
+
+// // letは際宣言不可
+// // let val2 = "let変数数再宣言"
+// // console.log(val2)
+
+// const val3 = "const変数"
+// console.log(val3)
+
+// const変数は上書き不可
+// val3 = "const変数を上書き"
+
+// const val3 = "const変数を際宣言"
+
+// オブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "新井",
+//   age: 28,
+// }
+// val4.name = "前田"
+// val4.addres = "広島"
+// console.log(val4)
+
+const val5 = ["dog", "cat"];
+console.log(val5);
+
+val5[0] = "bird";
+val5.push("monkey");
